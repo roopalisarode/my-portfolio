4c952248288fe18dfcfcae624b5c1814da161b33
roopali <rs366@njit.edu>
2023-05-02 23:36:14 -0400

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: "MyWebClass.org Simulation project",
+    subtitle: "HTML, CSS, JavaScript, Docker, Playwright, Webpack, Git, DevOps",
+    description:
+      "A website for educators to collaborate for learning web systems technologies.",
+    image: "./project_mywebclass.jpg",
+    link: "https://roopalisarode.github.io/mywebclass-simulation/",
+  },
+  {
+    title: "Vehicle detection and tracking in a video using Gaussian Mixture model",
+    subtitle: "Python",
+    description:
+      "A detection and segmentation technique followed by a contour tracking method is used to detect and keep track of vehicles passing by in a traffic video. On the basis of this division, further segmentation and tracking techniques are applied to detect an object.",
+    image: "./project_placeholder.jpg",
+    link: "https://github.com/roopalisarode/Image-Processing-Vehicle-detection",
+  },
+  {
+    title: "Naïve Bayes and Decision Tree Classification",
+    subtitle: "Python (sklearn, pandas)",
+    description:
+      "Implemented Naïve Bayes and Decision Tree classifier on the mushroom dataset to classify the mushroom into edible and poisonous category. Compared the algorithms based on training accuracy, training precision and F1 Score.",
+    image: "./project_decisiontree.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Apriori algorithm implementation",
+    subtitle: "Python",
+    description:
+      "Frequent item set mining and association rule learning over various datasets using Apriori algorithm and compared it with brute force approach using CPU runtime parameter and accuracy of the sets mined.",
+    image: "./project_placeholder.jpg",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const skills = [
+  "Python",
+  "JavaScript",
+  "React",
+  "Express",
+  "Mongo DB",
+  "Next.js",
+  "Node",
+  "SQL",
+  "Java",
+  "Docker"
+];
